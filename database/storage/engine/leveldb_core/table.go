52af73a6a2f3dc08081297248ab8b8bcfc918d93
superp00t <superp00t@tutanota.com>
2024-01-31 12:18:12 -0500

    refactor(database): clean up unused code

--- database/storage/engine/leveldb_core/table.go
+++ database/storage/engine/leveldb_core/table.go
@@ -3,8 +3,7 @@ package leveldb_core
 import "sync"
 
 type table struct {
-	id          int32
-	info        table_info
-	guard_info  sync.Mutex
-	guard_index sync.Mutex
+	id         int32
+	info       table_info
+	guard_info sync.Mutex
 }
--- database/storage/engine/leveldb_core/table_index.go
+++
@@ -1,9 +0,0 @@
-package leveldb_core
-
-func (table *table) lock_indices() {
-	table.guard_index.Lock()
-}
-
-func (table *table) unlock_indices() {
-	table.guard_index.Unlock()
-}
