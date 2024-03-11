00f8dc75c440884f4f0a8019d1e5265e5048493b
superp00t <superp00t@tutanota.com>
2024-03-11 19:22:12 -0400

    fix(database): treat time.Time as numeric type

--- database/query/numeric.go
+++ database/query/numeric.go
@@ -1,7 +1,9 @@
 package query
 
+import "time"
+
 type Numeric interface {
-	~int | ~int8 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64
+	~int | ~int8 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | time.Time
 }
 
 type Integer interface {
