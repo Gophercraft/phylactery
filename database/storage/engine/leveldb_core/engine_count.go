0f7501935941878382fd36921a2b345f285e7a7f
superp00t <superp00t@tutanota.com>
2024-03-19 16:14:35 -0400

    fix(leveldb_core): always get table instance before counting records

--- database/storage/engine/leveldb_core/engine_count.go
+++ database/storage/engine/leveldb_core/engine_count.go
@@ -7,11 +7,13 @@ import (
 
 func (engine *engine) Count(table_id int32, expr *query.Expression) (count uint64, err error) {
 	var table *table
+
+	table, err = engine.get_table(table_id)
+	if err != nil {
+		return
+	}
+
 	if len(expr.Conditions) == 0 {
-		table, err = engine.get_table(table_id)
-		if err != nil {
-			return
-		}
 		table.lock_info()
 		count = table.info.rows
 		table.unlock_info()
