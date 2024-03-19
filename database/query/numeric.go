3286aea6e0e5f3a2981f8c25e00a5d8a40bf5e36
superp00t <superp00t@tutanota.com>
2024-03-19 17:15:57 -0400

    fix(query): comment typo

--- database/query/numeric.go
+++ database/query/numeric.go
@@ -2,7 +2,7 @@ package query
 
 import "time"
 
-// Constrains types that can be said to be greater or less than other values of the same time
+// Constrains types that can be said to be greater or less than other values of the same type
 type Numeric interface {
 	~int | ~int8 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~float32 | ~float64 | time.Time
 }
