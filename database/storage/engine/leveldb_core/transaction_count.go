4a717e44919a177098702a207e7f2b8c81707604
superp00t <superp00t@tutanota.com>
2024-03-19 16:45:49 -0400

    fix(leveldb_core): always get table instance before counting records (in transaction, too)

--- database/storage/engine/leveldb_core/transaction_count.go
+++ database/storage/engine/leveldb_core/transaction_count.go
@@ -6,11 +6,11 @@ import (
 
 func (tx *transaction) Count(table_id int32, expr *query.Expression) (count uint64, err error) {
 	var table *table
+	table, err = tx.engine.get_table(table_id)
+	if err != nil {
+		return
+	}
 	if len(expr.Conditions) == 0 {
-		table, err = tx.engine.get_table(table_id)
-		if err != nil {
-			return
-		}
 		table.lock_info()
 		count = table.info.rows
 		table.unlock_info()
